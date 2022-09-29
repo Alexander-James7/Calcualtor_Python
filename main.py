883eec3ca298fdf079b6dbcceb45594496e95268
Alexander-James7 <113034143+Alexander-James7@users.noreply.github.com>
2022-09-29 09:58:46 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+print ("Let's find the area of a trapezoid!")
+base_a = input("Enter Base A in cm")
+base_b = input("Enter Base B in cm")
+height = input("Enter height in cm")
+a_b = base_a + base_b
+a_b2 = a_b / int (2)
