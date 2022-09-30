b1eb929059a90326c15902b282531b0279f0fed7
Alexander-James7 <113034143+Alexander-James7@users.noreply.github.com>
2022-09-30 10:01:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 print ("Let's find the area of a trapezoid!")
-base_a = input("Enter Base A in cm")
-base_b = input("Enter Base B in cm")
-height = input("Enter height in cm")
-a_b = int(base_a) + int(base_b)
-ab2 = int(a_b) / int(2)
-final = int(ab2) * int(height)
+base_a = int(input("Enter Base A in cm"))
+base_b = int(input("Enter Base B in cm"))
+height = int(input("Enter height in cm"))
+a_b = base_a + base_b
+ab2 = a_b / 2
+final = ab2 * height
 print("Your final answer is!")
 print(final)
